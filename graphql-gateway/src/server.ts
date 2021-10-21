3d3e318da615bb7a2451027b15dfff18cbe771d9
luishporras <luishercep@gmail.com>
2021-10-21 13:18:55 +0200

    Add API gateway to implement microservice architecture with GraphQL

--- graphql-gateway/src/server.ts
+++ graphql-gateway/src/server.ts
@@ -1,16 +1,52 @@
-import ApolloServer from 'apollo-server'
-import ApolloGateway from '@apollo/gateway'
+import { introspectSchema } from '@graphql-tools/wrap'
+import { stitchSchemas } from '@graphql-tools/stitch'
+import { ApolloServer } from 'apollo-server'
+import makeRemoteExecutor from './utils/makeRemoteExecutor'
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground
+} from "apollo-server-core";
 
-const supergraphSdl = ''; // TODO!
+async function makeGatewaySchema() {
+  const usersRemoteExecutor = makeRemoteExecutor(
+    'http://localhost:4001/'
+  )
+  const blockchainRemoteExecutor = makeRemoteExecutor(
+    'http://localhost:4002/'
+  )
+  const adminContext = { authHeader: 'Commons my-app-to-app-token' };
 
-const gateway = new ApolloGateway({
-  supergraphSdl
-});
+  return stitchSchemas({
+    subschemas: [
+      {
+        schema: await introspectSchema(usersRemoteExecutor, adminContext),
+        executor: usersRemoteExecutor,
+      },
+      {
+        schema: await introspectSchema(blockchainRemoteExecutor, adminContext),
+        executor: blockchainRemoteExecutor,
+      },
+    ],
+  })
+}
 
-const server = new ApolloServer({
-  gateway,
-});
+makeGatewaySchema().then( schema => {
+  const server = new ApolloServer({ 
+    schema: schema, 
+    context: (query) => {
+      return {
+        authHeader:  query.req.headers.authorization || '',
+      }
+    },
+    // Add this to change landing page, necesary for this version of ApolloServer
+    plugins: [
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  })
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Gateway ready at ${url}`);
-}).catch(err => {console.error(err)});
+  const port = 4000
+
+  server.listen(port).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+  })
+})
+
